a9a151e926cb4f397391dacdb18eb2a51333e2cd
Ian Thomson <i.thomson@ljmu.ac.uk>
2020-12-09 00:55:42 +0000

    added italian cypress to list of models

--- dist-server/model/models.js
+++ dist-server/model/models.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 exports.models = void 0;
 var models = [{
   id: 1,
+  type: "static",
   name: "Bandstand",
   notes: "Imported from blender",
   path: "/models/",
@@ -13,6 +14,7 @@ var models = [{
   thumb: "/img/thumb/models/bandstand.png"
 }, {
   id: 2,
+  type: "static",
   name: "bench",
   notes: "A picnic bench",
   path: "/models/",
@@ -20,6 +22,7 @@ var models = [{
   thumb: "/img/thumb/models/bench.png"
 }, {
   id: 3,
+  type: "static",
   name: "Tree 12m",
   notes: "Imported from blender",
   path: "/models/",
@@ -27,6 +30,7 @@ var models = [{
   thumb: "/img/thumb/models/tree.png"
 }, {
   id: 5,
+  type: "static",
   name: "Tree 8m",
   notes: "A picnic bench",
   path: "/models/",
@@ -34,6 +38,7 @@ var models = [{
   thumb: "/img/thumb/models/tree.png"
 }, {
   id: 6,
+  type: "static",
   name: "Tree 6m",
   notes: "Imported from blender",
   path: "/models/",
@@ -41,6 +46,7 @@ var models = [{
   thumb: "/img/thumb/models/tree.png"
 }, {
   id: 7,
+  type: "static",
   name: "Shed 2",
   notes: "A simple shed",
   path: "/models/",
@@ -48,6 +54,7 @@ var models = [{
   thumb: "/img/thumb/models/shed1.png"
 }, {
   id: 8,
+  type: "static",
   name: "Gum Tree",
   notes: "imported from sweethome3d.com",
   path: "/models/",
@@ -55,6 +62,7 @@ var models = [{
   thumb: "/img/thumb/models/gumtree15m.png"
 }, {
   id: 9,
+  type: "static",
   name: "Gum Tree 2",
   notes: "imported from sweethome3d.com",
   path: "/models/",
@@ -62,6 +70,7 @@ var models = [{
   thumb: "/img/thumb/models/gumtree2-15m.png"
 }, {
   id: 12,
+  type: "static",
   name: "Male",
   notes: "A male",
   path: "/models/",
@@ -69,6 +78,7 @@ var models = [{
   thumb: "/img/thumb/models/male.png"
 }, {
   id: 13,
+  type: "static",
   name: "Birch",
   notes: "Imported from blender",
   path: "/models/birch3/",
@@ -76,6 +86,7 @@ var models = [{
   thumb: "/img/thumb/models/tree.png"
 }, {
   id: 14,
+  type: "static",
   name: "hut",
   notes: "a hut",
   path: "/models/hut/",
@@ -83,6 +94,7 @@ var models = [{
   thumb: "/img/thumb/models/shed1.png"
 }, {
   id: 16,
+  type: "static",
   name: "Pine",
   notes: "Imported from Sweet Home 3D",
   path: "/models/pine/",
@@ -90,6 +102,7 @@ var models = [{
   thumb: "/img/thumb/models/pine.png"
 }, {
   id: 17,
+  type: "static",
   name: "Gazebo",
   notes: "A picnic bench",
   path: "/models/gazebo/",
@@ -97,6 +110,7 @@ var models = [{
   thumb: "/img/thumb/models/gazebo.png"
 }, {
   id: 18,
+  type: "static",
   name: "Greenhouse",
   notes: "Imported from blender",
   path: "/models/greenhouse/",
@@ -104,6 +118,7 @@ var models = [{
   thumb: "/img/thumb/models/greenhouse.png"
 }, {
   id: 19,
+  type: "static",
   name: "house1",
   notes: "A house",
   path: "/models/house1/",
@@ -111,6 +126,7 @@ var models = [{
   thumb: "/img/thumb/models/house.png"
 }, {
   id: 20,
+  type: "static",
   name: "slide",
   notes: "from sweethome 3d",
   path: "/models/child-slide/",
@@ -118,10 +134,33 @@ var models = [{
   thumb: "/img/thumb/models/child-slide.png"
 }, {
   id: 21,
+  type: "static",
   name: "goal",
   notes: "from sweethome 3d",
   path: "/models/goal/",
   file: "goal.babylon",
   thumb: "/img/thumb/models/goal.png"
+}, {
+  id: 22,
+  hstart: 1.5,
+  hend: 12,
+  base: 1.7,
+  years: 50,
+  type: "static",
+  name: "italian-cypress",
+  notes: "from tree generator",
+  path: "/models/italian-cypress/",
+  file: "italian-cypress.babylon",
+  thumb: "/img/thumb/models/italian-cypress.png"
+}, {
+  id: 23,
+  type: "grow",
+  name: "growbirchtest",
+  notes: "morphtarget test",
+  path: "/models/birch3",
+  base: "birch-young.babylon",
+  target: "birch-mature.babylon",
+  thumb: "/img/thumb/models/tree.png",
+  maturity: 50
 }];
 exports.models = models;
--- server/model/models.js
+++ server/model/models.js
@@ -1,137 +1,178 @@
 export const models = [{
-    id: 1,
-    name: "Bandstand",
-    notes: "Imported from blender",
-    path: "/models/",
-    file: "bandstand.babylon",
-    thumb: "/img/thumb/models/bandstand.png"
-},
-{
-    id: 2,
-    name: "bench",
-    notes: "A picnic bench",
-    path: "/models/",
-    file: "bench.babylon",
-    thumb: "/img/thumb/models/bench.png"
-},
-{
-    id: 3,
-    name: "Tree 12m",
-    notes: "Imported from blender",
-    path: "/models/",
-    file: "tree12m.babylon",
-    thumb: "/img/thumb/models/tree.png"
-},
-{
-    id: 5,
-    name: "Tree 8m",
-    notes: "A picnic bench",
-    path: "/models/",
-    file: "tree8m.babylon",
-    thumb: "/img/thumb/models/tree.png"
-},
-{
-    id: 6,
-    name: "Tree 6m",
-    notes: "Imported from blender",
-    path: "/models/",
-    file: "tree6m.babylon",
-    thumb: "/img/thumb/models/tree.png"
-},
-{
-    id: 7,
-    name: "Shed 2",
-    notes: "A simple shed",
-    path: "/models/",
-    file: "shed2.babylon",
-    thumb: "/img/thumb/models/shed1.png"
-},
-{
-    id: 8,
-    name: "Gum Tree",
-    notes: "imported from sweethome3d.com",
-    path: "/models/",
-    file: "gumtree15m.babylon",
-    thumb: "/img/thumb/models/gumtree15m.png"
-},
-{
-    id: 9,
-    name: "Gum Tree 2",
-    notes: "imported from sweethome3d.com",
-    path: "/models/",
-    file: "gumtree2-15m.babylon",
-    thumb: "/img/thumb/models/gumtree2-15m.png"
-},
-{
-    id: 12,
-    name: "Male",
-    notes: "A male",
-    path: "/models/",
-    file: "male.babylon",
-    thumb: "/img/thumb/models/male.png"
-},
-{
-    id: 13,
-    name: "Birch",
-    notes: "Imported from blender",
-    path: "/models/birch3/",
-    file: "birch.babylon",
-    thumb: "/img/thumb/models/tree.png"
-},
-{
-    id: 14,
-    name: "hut",
-    notes: "a hut",
-    path: "/models/hut/",
-    file: "hut2.babylon",
-    thumb: "/img/thumb/models/shed1.png"
-},
-{
-    id: 16,
-    name: "Pine",
-    notes: "Imported from Sweet Home 3D",
-    path: "/models/pine/",
-    file: "pine.babylon",
-    thumb: "/img/thumb/models/pine.png"
-},
-{
-    id: 17,
-    name: "Gazebo",
-    notes: "A picnic bench",
-    path: "/models/gazebo/",
-    file: "gazebo.babylon",
-    thumb: "/img/thumb/models/gazebo.png"
-},
-{
-    id: 18,
-    name: "Greenhouse",
-    notes: "Imported from blender",
-    path: "/models/greenhouse/",
-    file: "greenhouse.babylon",
-    thumb: "/img/thumb/models/greenhouse.png"
-},
-{
-    id: 19,
-    name: "house1",
-    notes: "A house",
-    path: "/models/house1/",
-    file: "house1.babylon",
-    thumb: "/img/thumb/models/house.png"
-},
-{
-    id: 20,
-    name: "slide",
-    notes: "from sweethome 3d",
-    path: "/models/child-slide/",
-    file: "child-slide.babylon",
-    thumb: "/img/thumb/models/child-slide.png"
-},
-{
-    id: 21,
-    name: "goal",
-    notes: "from sweethome 3d", 
-    path: "/models/goal/",
-    file: "goal.babylon",
-    thumb: "/img/thumb/models/goal.png"
-}
+        id: 1,
+        type: "static",
+        name: "Bandstand",
+        notes: "Imported from blender",
+        path: "/models/",
+        file: "bandstand.babylon",
+        thumb: "/img/thumb/models/bandstand.png"
+    },
+    {
+        id: 2,
+        type: "static",
+        name: "bench",
+        notes: "A picnic bench",
+        path: "/models/",
+        file: "bench.babylon",
+        thumb: "/img/thumb/models/bench.png"
+    },
+    {
+        id: 3,
+        type: "static",
+        name: "Tree 12m",
+        notes: "Imported from blender",
+        path: "/models/",
+        file: "tree12m.babylon",
+        thumb: "/img/thumb/models/tree.png"
+    },
+    {
+        id: 5,
+        type: "static",
+        name: "Tree 8m",
+        notes: "A picnic bench",
+        path: "/models/",
+        file: "tree8m.babylon",
+        thumb: "/img/thumb/models/tree.png"
+    },
+    {
+        id: 6,
+        type: "static",
+        name: "Tree 6m",
+        notes: "Imported from blender",
+        path: "/models/",
+        file: "tree6m.babylon",
+        thumb: "/img/thumb/models/tree.png"
+    },
+    {
+        id: 7,
+        type: "static",
+        name: "Shed 2",
+        notes: "A simple shed",
+        path: "/models/",
+        file: "shed2.babylon",
+        thumb: "/img/thumb/models/shed1.png"
+    },
+    {
+        id: 8,
+        type: "static",
+        name: "Gum Tree",
+        notes: "imported from sweethome3d.com",
+        path: "/models/",
+        file: "gumtree15m.babylon",
+        thumb: "/img/thumb/models/gumtree15m.png"
+    },
+    {
+        id: 9,
+        type: "static",
+        name: "Gum Tree 2",
+        notes: "imported from sweethome3d.com",
+        path: "/models/",
+        file: "gumtree2-15m.babylon",
+        thumb: "/img/thumb/models/gumtree2-15m.png"
+    },
+    {
+        id: 12,
+        type: "static",
+        name: "Male",
+        notes: "A male",
+        path: "/models/",
+        file: "male.babylon",
+        thumb: "/img/thumb/models/male.png"
+    },
+    {
+        id: 13,
+        type: "static",
+        name: "Birch",
+        notes: "Imported from blender",
+        path: "/models/birch3/",
+        file: "birch.babylon",
+        thumb: "/img/thumb/models/tree.png"
+    },
+    {
+        id: 14,
+        type: "static",
+        name: "hut",
+        notes: "a hut",
+        path: "/models/hut/",
+        file: "hut2.babylon",
+        thumb: "/img/thumb/models/shed1.png"
+    },
+    {
+        id: 16,
+        type: "static",
+        name: "Pine",
+        notes: "Imported from Sweet Home 3D",
+        path: "/models/pine/",
+        file: "pine.babylon",
+        thumb: "/img/thumb/models/pine.png"
+    },
+    {
+        id: 17,
+        type: "static",
+        name: "Gazebo",
+        notes: "A picnic bench",
+        path: "/models/gazebo/",
+        file: "gazebo.babylon",
+        thumb: "/img/thumb/models/gazebo.png"
+    },
+    {
+        id: 18,
+        type: "static",
+        name: "Greenhouse",
+        notes: "Imported from blender",
+        path: "/models/greenhouse/",
+        file: "greenhouse.babylon",
+        thumb: "/img/thumb/models/greenhouse.png"
+    },
+    {
+        id: 19,
+        type: "static",
+        name: "house1",
+        notes: "A house",
+        path: "/models/house1/",
+        file: "house1.babylon",
+        thumb: "/img/thumb/models/house.png"
+    },
+    {
+        id: 20,
+        type: "static",
+        name: "slide",
+        notes: "from sweethome 3d",
+        path: "/models/child-slide/",
+        file: "child-slide.babylon",
+        thumb: "/img/thumb/models/child-slide.png"
+    },
+    {
+        id: 21,
+        type: "static",
+        name: "goal",
+        notes: "from sweethome 3d",
+        path: "/models/goal/",
+        file: "goal.babylon",
+        thumb: "/img/thumb/models/goal.png"
+    },
+    {
+        id: 22,
+        hstart: 1.5,
+        hend: 12,
+        base: 1.7,
+        years: 50,  
+        type: "static",
+        name: "italian-cypress",
+        notes: "from tree generator",
+        path: "/models/italian-cypress/",
+        file: "italian-cypress.babylon",
+        thumb: "/img/thumb/models/italian-cypress.png"
+    },
+    {
+        id: 23,
+        type: "grow",
+        name: "growbirchtest",
+        notes: "morphtarget test",
+        path: "/models/birch3",
+        base: "birch-young.babylon",
+        target: "birch-mature.babylon",
+        thumb: "/img/thumb/models/tree.png",
+        maturity: 50
+    }
 ];
